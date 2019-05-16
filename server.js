491a5b7def5aff010f0f8b56a65b9fac20f9b10d
Omar Hossam <moar.ahmed@gmail.com>
2019-05-16 17:23:26 +0200

    backup

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({dest: __dirname + '/uploads/images'});
+// const upload = multer({dest: __dirname + '/uploads/images'});
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.static('public'));
 
-app.post('/upload', upload.single('photo'), (req, res) => {
-    if(req.file) {
-        res.json(req.file);
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/images')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now())
     }
-    else throw 'error';
+});
+
+var upload = multer({   storage: storage,
+                        limits: { fileSize: '50mb' }}).single('photo');
+
+app.post('/upload', upload.single('photo'), (req, res) => {
+    upload(req,res,function(err) {
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
 });
 
 app.listen(PORT, () => {
